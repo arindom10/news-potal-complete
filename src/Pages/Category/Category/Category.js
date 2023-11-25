29f9e6e4b3e8c75ca62caa1a38510acd70903c9d
arindom10 <karindom7@gmail.com>
2023-11-25 10:49:18 +0600

    Use Title Updated

--- src/Pages/Category/Category/Category.js
+++ src/Pages/Category/Category/Category.js
@@ -1,20 +1,19 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
-import NewsSummaryCard from '../../Shared/NewsSummaryCard/NewsSummaryCard';
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import NewsSummaryCard from "../../Shared/NewsSummaryCard/NewsSummaryCard";
+import useTitle from "../../../Hooks/useTitle";
 
 const Category = () => {
-    const categoryNews = useLoaderData();
-    return (
-        <div>
-            <h2>This is Category has news: {categoryNews.length}</h2>
-            {
-                categoryNews.map(news =><NewsSummaryCard
-                    key={news._id}
-                    news={news}
-                ></NewsSummaryCard>)
-            }
-        </div>
-    );
+  const categoryNews = useLoaderData();
+  useTitle("Category");
+  return (
+    <div>
+      <h2>This is Category has news: {categoryNews.length}</h2>
+      {categoryNews.map((news) => (
+        <NewsSummaryCard key={news._id} news={news}></NewsSummaryCard>
+      ))}
+    </div>
+  );
 };
 
 export default Category;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
 import NewsSummaryCard from "../../Shared/NewsSummaryCard/NewsSummaryCard";
+import useTitle from "../../../Hooks/useTitle";
 
 const Home = () => {
-    const allNews = useLoaderData();
-    return (
-        <div>
-            <h2>Dragon News Home: {allNews.length}</h2>
-            {allNews.map((news) => (
-                <NewsSummaryCard key={news._id} news={news}></NewsSummaryCard>
-            ))}
-        </div>
-    );
+  const allNews = useLoaderData();
+  useTitle("Home");
+  return (
+    <div>
+      <h2>Dragon News Home: {allNews.length}</h2>
+      {allNews.map((news) => (
+        <NewsSummaryCard key={news._id} news={news}></NewsSummaryCard>
+      ))}
+    </div>
+  );
 };
 
 export default Home;
